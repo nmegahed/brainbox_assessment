2d467f963aa4e672f6adebad12d365f0e86db6f2
Nour <nour.m.megahed83@gmail.com>
2025-06-14 09:00:55 -0400

    Update provider.tf

--- orange_blue/us-east-1/project_orange_blue/provider.tf
+++ orange_blue/us-east-1/project_orange_blue/provider.tf
@@ -1,9 +1,19 @@
+
+locals {
+  assume_role_arns = {
+    dev  = "arn:aws:iam::111111111111:role/TerraformExecutionRole"
+    test = "arn:aws:iam::222222222222:role/TerraformExecutionRole"
+    prod = "arn:aws:iam::333333333333:role/TerraformExecutionRole"
+  }
+  role_to_assume = local.assume_role_arns[terraform.]
+}
+
+
 provider "aws" {
   region = var.aws_region
 
-
- assume_role {
-    role_arn     = var.assume_role_arn
-    session_name = "terraform-session"
+  assume_role {
+    role_arn     = local.role_to_assume
+    session_name = "terraform-${terraform.workspace}"
   }
 }
