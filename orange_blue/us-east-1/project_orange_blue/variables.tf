1cc039d581ddd18a5c75c786d73c6c0c38560ffb
Nour <nour.m.megahed83@gmail.com>
2025-06-14 08:48:10 -0400

    Update variables.tf

--- orange_blue/us-east-1/project_orange_blue/variables.tf
+++ orange_blue/us-east-1/project_orange_blue/variables.tf
@@ -3,3 +3,10 @@ variable "aws_region" {
     default = "us-east-1"
     description = "The AWS Region to be used for the provider"
 }
+
+
+variable "assume_role" {
+    type = string
+    default = ""
+    description = "The AWS Role for AWS account that terraform can assume"
+}
